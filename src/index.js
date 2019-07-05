71bdec6e77eddfa0138bed85ce39c2e41fcfa25c
Bruceelei <1311563459@qq.com>
2019-07-05 16:56:56 +0800

    write sgmobal components

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ $('select.dropdown').dropdown();
 $(".ui.sgcard .icon.close").click(function () {
   $(this).closest(".ui.sgcard").transition("fade");
 });
+$(document).ready(function(){
+    $(".image.field").dimmer('show');
+    $(".image.field").click(function () {
+    $(".image.field").dimmer('hide');
+    $("img").attr("src", "image/tom.jpg");
+      $(".image.field").click(function(){
+        $("img").attr("src", "image/nan.jpg");
+      })
+  });
+});
