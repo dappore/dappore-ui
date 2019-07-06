6d20183790ab744f4a4d20bbcd515ebec729ee08
Bruceelei <1311563459@qq.com>
2019-07-06 12:21:26 +0800

    m

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ $('select.dropdown').dropdown();
 $(".ui.sgcard .icon.close").click(function () {
   $(this).closest(".ui.sgcard").transition("fade");
 });
-// document.onload(function(){
-//   var x=document.getElementsByClassName(".ui.field .five.cols .ui.sgcard");
-//   var array=[];
-//   for(var i=0;i<x.length,i++;){
-//      if(i>=5){
-//          x.
-//      }
-//   }
-// })
+$(document).ready(function(){
+    $(".image.field").dimmer('show');
+    $(".image.field").click(function () {
+    $(".image.field").dimmer('hide');
+    $("img").attr("src", "image/tom.jpg");
+      $(".image.field").click(function(){
+        $("img").attr("src", "image/nan.jpg");
+      })
+  });
+});
