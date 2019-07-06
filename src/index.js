217124c8cfa713241caf060474e78306f58acadf
Bruceelei <1311563459@qq.com>
2019-07-06 12:15:35 +0800

    m

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ $('select.dropdown').dropdown();
 $(".ui.sgcard .icon.close").click(function () {
   $(this).closest(".ui.sgcard").transition("fade");
 });
+// document.onload(function(){
+//   var x=document.getElementsByClassName(".ui.field .five.cols .ui.sgcard");
+//   var array=[];
+//   for(var i=0;i<x.length,i++;){
+//      if(i>=5){
+//          x.
+//      }
+//   }
+// })
