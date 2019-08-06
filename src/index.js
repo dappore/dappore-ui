beecf3a9f7e988c6a45f21850688748ead47b5ab
zkrao <kzr@zheng-chain.com>
2019-08-06 18:43:13 +0800

    add modal tab header.

--- src/index.js
+++ src/index.js
@@ -14,3 +14,30 @@ $('.ui.longer.modal')
 $(".ui.sgcard .icon.close").click(function () {
   $(this).closest(".ui.sgcard").transition("fade");
 });
+
+// tab select
+window.onload = function () {
+    var headers = $(".ui.infor.modal > .tab.header > ul > li");
+    var contents = $(".ui.infor.modal > .tab.content > div");
+
+    for (var i = 0; i < headers.length; i++) {
+        if(headers[i].className == "active") {
+            for (var j = 0; j < contents.length; j++) {
+                contents[j].className = "tab hide";
+            }
+            contents[i].className = "tab show";
+        }
+
+        headers[i].index = i;
+        headers[i].onclick = function () {
+            for (var i = 0; i < headers.length; i++) {
+                headers[i].className = "";
+            }
+            this.className = "active";
+            for (var j = 0; j < contents.length; j++) {
+                contents[j].className = "tab hide";
+            }
+            contents[this.index].className = "tab show";
+        }
+    }
+}
