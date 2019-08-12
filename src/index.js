f37a2a9e31e4f6625dcbb10d1a0ca0e340a7e1ac
Bruceelei <1311563459@qq.com>
2019-08-12 17:56:15 +0800

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 console.log('test');
-
-$(".ui.checkbox").checkbox();
-
 $("select.dropdown").dropdown();
-
-$(".ui.infor.modal").modal("show");
-
-$(".ui.infor.modal>.ui.form>radio.field>.inline.fields>.field>.ui.radio.checkbox").checkbox();
-$('.ui.longer.modal')
-    .modal('show')
-    ;
 // .ui.sgcard
 $(".ui.sgcard .icon.close").click(function () {
   $(this).closest(".ui.sgcard").transition("fade");
